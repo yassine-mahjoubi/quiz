9d40cf23d5153c37578ab84f2c74cbc8a00527ec
yassine mahjoubi <mahjoubi.sfx@gmail.com>
2025-08-13 23:18:37 +0200

    feat(logo)add icon package

---
+++ src/components/ui/BaseLogo.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+</script>
+<template>
+  <Icon aria-hidden="true" icon="emojione-monotone:victory-hand" width="64" height="64" />
+</template>
