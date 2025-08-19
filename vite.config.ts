fd50854b738d1c226bd1294e9f1c165f3145b35e
yassine mahjoubi <mahjoubi.sfx@gmail.com>
2025-08-19 22:26:58 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: process.env.NODE_ENV === 'production' ? '/quiz/' : '/',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
