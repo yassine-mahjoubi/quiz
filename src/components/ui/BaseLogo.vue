3deee171bd7ed175f04f8bf310d16e50a1d639e1
yassine mahjoubi <mahjoubi.sfx@gmail.com>
2025-10-01 19:19:22 +0200

    fix: color logo

--- src/components/ui/BaseLogo.vue
+++ src/components/ui/BaseLogo.vue
@@ -2,5 +2,13 @@
 import { Icon } from '@iconify/vue'
 </script>
 <template>
-  <Icon aria-hidden="true" icon="game-icons:wisdom" width="64" height="64" />
+  <Icon aria-hidden="true" class="icon" icon="game-icons:wisdom" width="64" height="64" />
 </template>
+<style lang="scss">
+.icon {
+  color: #fff;
+  &:hover {
+    color: #00b478;
+  }
+}
+</style>
