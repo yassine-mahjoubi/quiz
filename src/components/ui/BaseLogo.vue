bc26afcacc7dd949f4f6a95b0b399f756e9e9a7c
yassine mahjoubi <mahjoubi.sfx@gmail.com>
2025-11-11 19:00:02 +0100

    feat(logo):add switcher mode theme with preferenced user

--- src/components/ui/BaseLogo.vue
+++ src/components/ui/BaseLogo.vue
@@ -1,14 +1,47 @@
 <script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+const { t } = useI18n()
+import { computed, ref, watchEffect } from 'vue'
 import { Icon } from '@iconify/vue'
+
+const dataTheme: HTMLElement | null = document.querySelector('html')
+const savedThemeChoice = localStorage.getItem('mode')
+const startWithDarkMode = savedThemeChoice === 'dark'
+const enabledDarkMode = ref<boolean>(startWithDarkMode)
+
+watchEffect(() => {
+  const newThemeChoice = enabledDarkMode.value ? 'dark' : 'light'
+  dataTheme?.setAttribute('data-theme', newThemeChoice)
+  localStorage.setItem('mode', newThemeChoice)
+})
+const toggleMode = () => {
+  enabledDarkMode.value = !enabledDarkMode.value
+}
+const labelThemeTranslated = computed(() => {
+  return (
+    t('common.mode.label') +
+    ' ' +
+    (enabledDarkMode.value ? t('common.mode.light') : t('common.mode.dark'))
+  )
+})
 </script>
 <template>
-  <Icon aria-hidden="true" class="icon" icon="game-icons:wisdom" width="64" height="64" />
+  <button
+    class="bt-icon"
+    :title="labelThemeTranslated"
+    :aria-label="labelThemeTranslated"
+    @click="toggleMode()"
+  >
+    <Icon aria-hidden="true" class="icon" icon="game-icons:wisdom" width="64" height="64" />
+  </button>
 </template>
 <style lang="scss" scoped>
+.bt-icon {
+  padding: 5px;
+  border-radius: 100%;
+}
 .icon {
-  color: #fff;
-  &:hover {
-    color: #00b478;
-  }
+  cursor: pointer;
+  color: currentColor;
 }
 </style>
